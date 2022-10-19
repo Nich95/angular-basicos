b3e8bf284e625b0ef1fe56cf7f31684da90fcafc
nich-dev <nich.dev95@gmail.com>
2022-10-19 12:30:17 -0300

    Mostrar listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -12,16 +12,33 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [ // a tener en cuenta que aqui se agregan objetos
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 8500
+    }
+  ]
+
   nuevo: Personaje = { // esto es un objeto de tipo Personaje
-    nombre: 'Trunks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
   cambiarNombre( event: any ) {
     console.log( event.target.value() );
   }
 
-  agregar(  ) {
-    console.log(this.nuevo);
+  agregar() {
+    if ( this.nuevo.nombre.trim().length === 0 ) { return; } // esto hace que se salga de este bloque de código 
+    console.log( this.nuevo );
+
+    // a tener en cuenta: el this.nuevo se esta utilizando para inicializar el formulario,
+    // tambien toma los valores del formulario gracias al ngModel por lo que insertamos el this.nuevo
+    this.personajes.push( this.nuevo );
+    this.nuevo = { nombre: '', poder: 0 }; // le damos los valores iniciales del objeto
   }
 }
