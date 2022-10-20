ce89d1cee459ca184d0ece7599f42166196c86ef
nich-dev <nich.dev95@gmail.com>
2022-10-20 15:28:10 -0300

    Separacion en componentes
    Se separaron en dos componentes distintos el que muestra los
    personajes y el que los agrega

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -25,22 +25,8 @@ export class MainPageComponent {
     }
   ]
 
-  nuevo: Personaje = { // esto es un objeto de tipo Personaje
-    nombre: '',
-    poder: 0
-  }
-
-  cambiarNombre( event: any ) {
-    console.log( event.target.value() );
-  }
-
-  agregar() {
-    if ( this.nuevo.nombre.trim().length === 0 ) { return; } // esto hace que se salga de este bloque de código 
-    console.log( this.nuevo );
-
-    // a tener en cuenta: el this.nuevo se esta utilizando para inicializar el formulario,
-    // tambien toma los valores del formulario gracias al ngModel por lo que insertamos el this.nuevo
-    this.personajes.push( this.nuevo );
-    this.nuevo = { nombre: '', poder: 0 }; // le damos los valores iniciales del objeto
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
   }
 }
