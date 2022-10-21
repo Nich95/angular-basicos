248ba611c6cc004ccc89bc7270b16a5c7c08808e
nich-dev <nich.dev95@gmail.com>
2022-10-21 13:10:05 -0300

    Centralizado de la informacion

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -16,29 +16,14 @@ import { DbzService } from '../services/dbz.service';
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [ // a tener en cuenta que aqui se agregan objetos
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 8500
-    }
-  ]
-
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
-  }
-
-  agregarNuevoPersonaje( argumento: Personaje) {
-    this.personajes.push(argumento);
   }
 
   // forma corta de definir propiedades
   // esto se conoce como una injeccion de dependecias
   // como este es el componente padre solo se inicializa una vez y los componentes hjijos utilizan el mismo,
   // al pasar info y modificar el servicio en el padre los hijos trabajan con la misma info
-  constructor( private dbzService: DbzService ) {}
+  constructor( ) {}
 }
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from "@angular/core";
 
+import { Personaje } from '../interfaces/dbz.interface';
+
 // En el servicio va toda la info y los metodos que se utilizan para interactuar con fuentes externas
 // para manupular es estado de la información de la app
 // se trabaja mucho con solicitudes http donde se llaman los endpoints de las apis
 @Injectable()
 export class DbzService {
 
+  // por standar las propoiedades privadas se marcan con un guión bajo
+  private _personajes: Personaje[] = [ // a tener en cuenta que aqui se agregan objetos
+  {
+    nombre: 'Goku',
+    poder: 15000
+  },
+  {
+    nombre: 'Vegeta',
+    poder: 8500
+  }
+];
+
+// se tiene que tener cuidado con esto porque javascript todo lo manda por referencia
+  get personajes(): Personaje[] { // le indicamos que retorna un arreglo de personajes
+    // los corchetes indican que es un arreglo
+    // "..." es el operador spread que dice
+    // "separa cada uno de los elementos del arreglo y crea uno nuevo"
+    // con esto rompre la referencia con el arreglo real
+    return [...this._personajes]; // esto es una buena constumbre de javascriopt
+  }
+
   constructor() {
     console.log('Servicio inicializado...');
   }
 }
-
